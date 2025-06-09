593ce4c3e27b4db8075740d9731fbfa0e6f2c9c9
Lara Pla <laraplamoreno@gmail.com>
2025-06-09 13:00:17 +0200

    build(all): create project structure

---
+++ src/main/java/femcloudapi/FemCloudApiApplication.java
@@ -0,0 +1,13 @@
+package femcloudapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FemCloudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FemCloudApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/femcloudapi/FemCloudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package femcloudapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FemCloudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
