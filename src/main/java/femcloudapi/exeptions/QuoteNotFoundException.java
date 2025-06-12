9676904298baed194f3800a1f9012a664e91610b
Lara Pla <laraplamoreno@gmail.com>
2025-06-12 12:24:50 +0200

    fix(QuoteNotFounException): fix class name

--- src/main/java/femcloudapi/exeptions/QuoteNotFoundException.java
+++ src/main/java/femcloudapi/exeptions/QuoteNotFoundException.java
@@ -4,8 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class QuoteNotFoundExeption extends RuntimeException {
-    public QuoteNotFoundExeption (String message) {
+public class QuoteNotFoundException extends RuntimeException {
+    public QuoteNotFoundException(String message) {
         super(message);
     }
 }
