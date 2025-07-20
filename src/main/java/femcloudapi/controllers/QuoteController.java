35c642672fbcdb19e5c1d21350d96fb11f81210d
Lara Pla <laraplamoreno@gmail.com>
2025-07-20 21:17:00 +0200

    refactor(QuoteController): add dto to CRUD methods

--- src/main/java/femcloudapi/controllers/QuoteController.java
+++ src/main/java/femcloudapi/controllers/QuoteController.java
@@ -1,7 +1,9 @@
 package femcloudapi.controllers;
 
-import femcloudapi.models.Quote;
+import femcloudapi.dtos.QuoteRequest;
+import femcloudapi.dtos.QuoteResponse;
 import femcloudapi.services.QuoteService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,39 +11,39 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/quotes")
 public class QuoteController {
-    private final QuoteService quoteService;
+    private final QuoteService QUOTE_SERVICE;
 
-    public QuoteController(QuoteService quoteService) {
-        this.quoteService = quoteService;
+    public QuoteController(QuoteService QUOTE_SERVICE) {
+        this.QUOTE_SERVICE = QUOTE_SERVICE;
     }
 
-    @GetMapping("/quotes")
-    public ResponseEntity<List<Quote>> getAllQuotes() {
-        List<Quote> quotes = quoteService.getAllQuotes();
-        return new ResponseEntity<List<Quote>>(quotes, HttpStatus.OK);
+    @GetMapping
+    public ResponseEntity<List<QuoteResponse>> getAllQuotes() {
+        return new ResponseEntity<>(QUOTE_SERVICE.getAllQuotes(), HttpStatus.OK);
     }
 
-    @GetMapping("/quotes/{id}")
-    public ResponseEntity<Quote> getQuoteById(@PathVariable Long id) {
-        Quote quoteById = quoteService.getQuoteById(id);
-        return new ResponseEntity<Quote>(quoteById, HttpStatus.OK);
+    @GetMapping("/{id}")
+    public ResponseEntity<QuoteResponse> getQuoteById(@PathVariable Long id) {
+        QuoteResponse quoteResponse = QUOTE_SERVICE.getQuoteById(id);
+        return new ResponseEntity<>(quoteResponse, HttpStatus.OK);
     }
 
-    @PostMapping("/quotes")
-    public ResponseEntity<Quote> addQuote(@RequestBody Quote newQuote) {
-        Quote createdQuote = quoteService.addQuote(newQuote);
-        return new ResponseEntity<Quote>(createdQuote, HttpStatus.CREATED);
+    @PostMapping
+    public ResponseEntity<QuoteResponse> addQuote(@Valid @RequestBody QuoteRequest quoteRequest) {
+        return new ResponseEntity<>(QUOTE_SERVICE.addQuote(quoteRequest), HttpStatus.CREATED);
     }
 
-    @PutMapping("/quotes/{id}")
-    public void updateQuote(@PathVariable Long id, @RequestBody Quote quote) {
-        quoteService.updateQuote(id, quote);
+    @PutMapping("/{id}")
+    public ResponseEntity<QuoteResponse> updateQuote(@PathVariable Long id, @Valid @RequestBody QuoteRequest quoteRequest) {
+        QuoteResponse updatedQuote = QUOTE_SERVICE.updateQuote(id, quoteRequest);
+        return new ResponseEntity<>(updatedQuote, HttpStatus.OK);
     }
 
-    @DeleteMapping("/quotes/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteQuote(@PathVariable Long id) {
-        quoteService.deleteQuote(id);
-        return ResponseEntity.noContent().build();
+        QUOTE_SERVICE.deleteQuote(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
