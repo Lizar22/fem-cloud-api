de6efab7126ac2af690fd6ade65ec0b780202dcc
Lara Pla <laraplamoreno@gmail.com>
2025-07-26 01:27:58 +0200

    docs(data.sql): remove file

--- src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO quotes (text, author, quote_year) VALUES
-('Education is the path to freedom', 'Aspasia of Miletus', 'circa 430 BC'),
-('I am no longer accepting the things I cannot change. I am changing the things I cannot accept', 'Angela Y. Davies', '1972'),
-('They said, ''You are a savage and dangerous woman.'' I am speaking the truth. And the truth is savage and dangerous', 'Nawal El Saadawi', '1975'),
-('Defend your right to think, because even thinking wrongly is better than not thinking at all', 'Hypatia of Alexandria', 'circa 400'),
-('Your silence won''t protect you', 'Audre Lorde', '1977'),
-('Woman is born free and remains equal to man in rights', 'Olympe de Gouges', '1791');
